d02724f43f2ebc7d3aeb7c1df114697f8d0dd41d
Forest Moher <fomoher@gmail.com>
2020-04-28 10:32:59 -0700

    Add first example of JavaScript

---
+++ JS/Basic-JavaScript-1.js
@@ -0,0 +1,27 @@
+alert("Hello, there!");
+
+var A = "Hello, again";
+
+window.alert(A);
+
+document.write("So I says to the guy, \"who does you think you are!?\"");
+
+document.write(" So then I gave him a pair of cement shoes" + ", and now he's sleeping with the fishes.");
+
+var B = "\"Wow Big Tony, " + "you sure showed that guy.\""
+
+document.write(B);
+
+var boss = "Big Tony", thug1 = "Fat Louis", thug2 = "Sly Sammy" 
+
+var boss = boss.fontcolor("red");
+
+var thug1 = thug1.fontcolor("blue");
+
+var thug2 = thug2.fontcolor("orange");
+
+document.write(" " + thug1 + " is fat, of course.");
+
+document.write(" " + boss + " " + thug1 + " " + thug2 + " ");
+
+document.write(2 + 1 + " lousy thugs"); 
