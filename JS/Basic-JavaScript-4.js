b6a1c07c2f415256ea989366ad15e96cd7435d20
Forest Moher <fomoher@gmail.com>
2020-04-29 12:52:33 -0700

    Add dictionary

---
+++ JS/Basic-JavaScript-4.js
@@ -0,0 +1,12 @@
+function dictionary() {
+    var Cake = {
+        Flour: 4 + "cups",
+        Eggs: 2 + "ea",
+        Salt: 1 + "tsp",
+        Water: 2 + "cups",
+        Water: 1 + "cup",
+
+    };
+    delete Cake.Flour;
+    document.getElementById("recipe").innerHTML = "Start with " + Cake.Flour + " of flour.";
+}
